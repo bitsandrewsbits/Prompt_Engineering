9d5afd983785b0740b04e824ec497b05aa1d07d7
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-09-13 23:26:05 +0300

    add functions for metadata saving, article saving into txt file

--- Knowledge_Integration_into_AI_RAG_Pipeline/data_source_ingestion.py
+++ Knowledge_Integration_into_AI_RAG_Pipeline/data_source_ingestion.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import json
 
 article_URL = "https://blog.dzencode.com/ru/illyuziya-kachestva-vash-sayt-idealen-pozdravlyaem-vy-tolko-chto-sozhgli-byudzhet/"
 
@@ -9,12 +10,13 @@ article_metadata = {'url': article_URL, 'lang': '', 'date': '', 'topic': ''}
 def main(URL: str):
     response = requests.get(URL)
     if response.status_code == 200:
-        article_html_page = get_article_html(response)
-        html_parser = BeautifulSoup(article_html_page, 'html.parser')
+        article_html = get_article_html(response)
+        html_parser = BeautifulSoup(article_html, 'html.parser')
         article_metadata['lang'] = get_article_lang(response)
         article_metadata['date'] = get_request_date(response)
         article_metadata['topic'] = get_article_topic(html_parser)
-        print(article_metadata)
+        save_metadata(article_metadata)
+        save_article_HTML_to_txt(article_html)
     else:
         print(f"Error fetching data from API. Status code: {response.status_code}")
         print(f"Response:", response.text)
@@ -35,6 +37,14 @@ def get_article_topic(html_parser):
 
 def get_article_html(response):
     return response.text
-    
+
+def save_metadata(metadata: dict):
+    with open('metadata.json', 'w') as mdf:
+        json.dump(metadata, mdf)
+
+def save_article_HTML_to_txt(article_html):
+    with open('article.txt', 'w') as article_f:
+        article_f.write(article_html)
+
 if __name__ == "__main__":
     main(article_URL)
