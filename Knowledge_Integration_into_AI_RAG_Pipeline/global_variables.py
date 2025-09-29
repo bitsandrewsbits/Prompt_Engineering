6b0cdc2d243bbe6921f4024d164b070743a70013
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-09-29 22:13:53 +0300

    add new file - global variables initialization.

---
+++ Knowledge_Integration_into_AI_RAG_Pipeline/global_variables.py
@@ -0,0 +1,16 @@
+ARTICLE_URL = "https://blog.dzencode.com/ru/illyuziya-kachestva-vash-sayt-idealen-pozdravlyaem-vy-tolko-chto-sozhgli-byudzhet/"
+ARTICLE_TXT_FILENAME = 'article.txt'
+ARTICLE_METADATA_FILENAME = 'metadata.json'
+ARTICLE_METADATA_URL_KEY = 'url'
+ARTICLE_METADATA_LANG_KEY = 'lang'
+ARTICLE_METADATA_DATE_KEY = 'date'
+ARTICLE_METADATA_TOPIC_KEY = 'topic'
+
+PREPROCESSED_ARTICLE_TEXT_FILENAME = 'cleaned_article.txt'
+ARTICLE_LANGUAGE = 'russian'
+
+CHUNKS_JSONL_FILENAME = 'rag_article.jsonl'
+CHUNK_SIZE_IN_TOKENS = 512
+CHUNK_METADATA_ID_KEY = 'chunk_ID'
+CHUNK_DATA_TEXT_KEY = 'chunk_text'
+CHUNK_DATA_METADATA_KEY = 'metadata'
