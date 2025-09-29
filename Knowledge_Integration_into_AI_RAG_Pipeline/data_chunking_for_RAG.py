610af6f5f5d28575e175422d7967e5cf629ce03b
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-09-29 22:20:27 +0300

    change token-level strategy to symbol token level. also change vars name to global variables.

--- Knowledge_Integration_into_AI_RAG_Pipeline/data_chunking_for_RAG.py
+++ Knowledge_Integration_into_AI_RAG_Pipeline/data_chunking_for_RAG.py
@@ -1,44 +1,50 @@
 # Data chunking for RAG
 import json
-
-preprocessed_article_text_file = 'cleaned_article.txt'
-article_metadata_file = 'metadata.json'
-chunks_JSONL_filename = 'rag_article.jsonl'
-
-chunk_size_in_tokens = 512
+from global_variables import *
 
 def main(preprocessed_article_file: str, chunk_size_in_tokens: int, metadata_file: str, chunks_JSONL_file: str):
-	preprocessed_article_text = get_preprocessed_article_text(preprocessed_article_file)
-	article_tokens = get_article_tokens(preprocessed_article_text)
-	metadata = get_metadata(metadata_file)
-	chunked_data = get_chunked_data(article_tokens, chunk_size_in_tokens, metadata)
-	save_chunks_into_JSONL(chunked_data, chunks_JSONL_file)
+    preprocessed_article_text = get_preprocessed_article_text(preprocessed_article_file)
+    article_words = get_article_words(preprocessed_article_text)
+    chunks_texts = get_chunks_texts(article_words, chunk_size_in_tokens)
+    metadata = get_metadata(metadata_file)
+    chunked_data = get_chunked_data(chunks_texts, metadata)
+    save_chunks_into_JSONL(chunked_data, chunks_JSONL_file)
 
 def get_preprocessed_article_text(filename: str):
 	with open(filename, 'r') as prep_txt_f:
 		preprocessed_article_text = prep_txt_f.read()
 		return preprocessed_article_text
 
-def get_article_tokens(article_text: str) -> list:
+def get_article_words(article_text: str) -> list:
 	return article_text.split(' ')
 
-def get_chunked_data(article_tokens: list, chunk_size_in_tokens: int, metadata: dict):
-	chunk_ID = 1
-	chunked_data = []
-	while len(article_tokens) != 0:
-		chunk_metadata = metadata.copy()
-		chunk_metadata['chunk_ID'] = chunk_ID
-		chunk = {'chunk_text': '', 'metadata': {}}
-		chunk['metadata'] = chunk_metadata
-		chunk['chunk_text'] = get_chunk_text(article_tokens, chunk_size_in_tokens)
-		chunked_data.append(chunk)
-		chunk_ID += 1
-	return chunked_data
+def get_chunked_data(chunks_texts: list[str], metadata: dict):
+    current_chunk_ID = 1
+    chunked_data = []
+    for (chunk_ID, chunk_text) in enumerate(chunks_texts, current_chunk_ID):
+        chunk_metadata = metadata.copy()
+        chunk_metadata[CHUNK_METADATA_ID_KEY] = chunk_ID
+        chunk = {CHUNK_DATA_TEXT_KEY: '', CHUNK_DATA_METADATA_KEY: chunk_metadata}
+        chunk[CHUNK_DATA_TEXT_KEY] = chunk_text
+        chunked_data.append(chunk)
+    return chunked_data
 
-def get_chunk_text(article_tokens: list, chunk_size_in_tokens: int):
-	if len(article_tokens) - chunk_size_in_tokens < chunk_size_in_tokens:
-		chunk_size_in_tokens = len(article_tokens)
-	return ' '.join([article_tokens.pop(0) for _ in range(chunk_size_in_tokens)])
+def get_chunks_texts(article_words: list, words_symbols_amount_for_chunks: int):
+    chunks_texts = []
+    while len(article_words) != 0:
+        chunk_words_remained_article_words = get_chunk_words(article_words, words_symbols_amount_for_chunks)
+        chunks_texts.append(' '.join(chunk_words_remained_article_words['chunk_words']))
+        article_words = chunk_words_remained_article_words['remained_article_words']
+    return chunks_texts
+        
+def get_chunk_words(article_words: list, words_symbols_amount_for_chunks: int):
+    chunk_words = []
+    while len(''.join(chunk_words)) < words_symbols_amount_for_chunks:
+        if len(''.join(article_words)) - words_symbols_amount_for_chunks < words_symbols_amount_for_chunks:
+            chunk_words = article_words
+            return {'chunk_words': chunk_words, 'remained_article_words': []}
+        chunk_words.append(article_words.pop(0))
+    return {'chunk_words': chunk_words, 'remained_article_words': article_words}
 
 def get_metadata(metadata_file: str):
 	with open(metadata_file, 'r') as metadata_f:
@@ -50,8 +56,8 @@ def save_chunks_into_JSONL(chunks: list, JSONL_file: str):
 		jsonl_f.write(serialized_jsonl_chunks)
 
 if __name__ == '__main__':
-	main(preprocessed_article_text_file, 
-		chunk_size_in_tokens, 
-		article_metadata_file,
-		chunks_JSONL_filename
+	main(PREPROCESSED_ARTICLE_TEXT_FILENAME,
+		CHUNK_SIZE_IN_TOKENS,
+		ARTICLE_METADATA_FILENAME,
+		CHUNKS_JSONL_FILENAME
 	)
