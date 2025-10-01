dd1d4d2e878c880a955a4757658170adc6cb6e0b
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-10-01 22:40:44 +0300

    change embed generation method - use langchain method. also add chunks embeddings saving to .pkl file

--- Knowledge_Integration_into_AI_RAG_Pipeline/generate_embeddings.py
+++ Knowledge_Integration_into_AI_RAG_Pipeline/generate_embeddings.py
@@ -1,15 +1,16 @@
 # Embeddings Generations using 
 #Sentence-BERT transformer pretrained model
 import json
-from sentence_transformers import SentenceTransformer
+import pickle
+from langchain_huggingface import HuggingFaceEmbeddings
+from global_variables import *
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-RAG_chunks_file = 'rag_article.jsonl'
+EMBEDDINGS_MODEL = HuggingFaceEmbeddings(model_name = f"sentence-transformers/{SENTENCE_TRANSFORMER_MODEL_NAME}")
 
-def main(chunks_file: str):
-	RAG_chunks_data = get_chunked_data(chunks_file)
-	add_chunked_data_embeddings_to_chunks(RAG_chunks_data)
-	print(RAG_chunks_data)
+def main(chunks_file, embed_model, embed_vectors_filename):
+    chunks = get_chunked_data(chunks_file)
+    chunks_texts_embedding_vectors = get_chunks_texts_embed_vectors(embed_model, chunks)
+    save_chunks_embedding_vectors(chunks_texts_embedding_vectors, embed_vectors_filename)
 
 def get_chunked_data(chunks_file: str) -> list[dict]:
 	loaded_chunked_data = []
@@ -18,10 +19,17 @@ def get_chunked_data(chunks_file: str) -> list[dict]:
 			loaded_chunked_data.append(json.loads(line))
 	return loaded_chunked_data
 
-def add_chunked_data_embeddings_to_chunks(chunks: list):
-	for chunk in chunks:	
-		chunk_embedding = model.encode(chunk['chunk_text'])
-		chunk['embedding'] = chunk_embedding
+def get_chunks_texts_embed_vectors(embed_model, chunks):
+    chunks_texts = [chunk[CHUNK_DATA_TEXT_KEY] for chunk in chunks]
+    return embed_model.embed_documents(chunks_texts)
 
+def save_chunks_embedding_vectors(embed_vectors, filename: str):
+    with open(filename, 'wb') as embed_vectors_f:
+        pickle.dump(embed_vectors, embed_vectors_f)
+    print(f'Embedding Vectors Saved into {filename}!')
+    
 if __name__ == '__main__':
-	main(RAG_chunks_file)
+	main(CHUNKS_JSONL_FILENAME,
+		EMBEDDINGS_MODEL,
+		EMBEDDING_VECTORS_FILENAME
+	)
